cd828b4f504103597346a8b76769afba8f1c2559
Purushothaman R <purushothamanramesh3@gmail.com>
2025-01-04 09:44:44 +0530

    feat: implement JWT authentication with login functionality, add guards for user routes, and configure Swagger for bearer token support

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from 'nestjs-prisma';
 import { ArticlesModule } from './articles/articles.module';
-import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     PrismaModule.forRoot({
+      isGlobal: true,
+    }),
+    ConfigModule.forRoot({
       isGlobal: true,
     }),
     ArticlesModule,
